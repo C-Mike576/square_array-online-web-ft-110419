43cd5bbaa548efb57d1688b1991b1015957e2393
Michael Calkins <m.calkins576@gmail.com>
2019-11-04 19:23:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
-  counter = 0
-  arr = Array.new(array.length)
-  while array[counter] do
-    arr[counter] = array[counter]**2
-    counter += 1
+  array.each do |n|
+    n**2
   end
-  array = arr
 end
